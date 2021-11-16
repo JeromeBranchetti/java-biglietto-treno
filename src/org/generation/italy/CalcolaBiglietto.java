4c8c0cff4a8dd85287da61aca656329a4bacfbc6
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2021-11-16 14:20:33 +0100

    source file biglietto treno

---
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -0,0 +1,44 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubù
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Inserire numeri chilometri viaggio: ");
+		double numKm = scan.nextDouble();
+		System.out.print("Quanti anni ha il passeggero: ");
+		int age = scan.nextInt();
+		double rate = 0.21;
+		double total;
+		
+		double basePrice = numKm * rate;
+		
+		if (age > 0 && age < 120) {
+			if (age < 18) {
+				total = basePrice * 0.80;
+			}
+			else if (age > 65) {
+				total = basePrice * 0.60;
+							
+			}
+			else {
+				total = basePrice;
+			}
+			System.out.print("Il costo totale del viaggio è: " + total + "€");
+			
+		}
+		else {
+			System.out.print("Inserisci una età valida");
+		}
+		
+		
+		
+	
+		scan.close();
+	}
+
+}
